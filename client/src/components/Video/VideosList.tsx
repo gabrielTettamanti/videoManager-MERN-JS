e496f8ba3b7908a5243532ce1bf8b494b4fc4f02
gabrielTettamanti <gabrieltettamanti.dev@gmail.com>
2022-11-24 12:07:15 -0300

    Created Client app

---
+++ client/src/components/Video/VideosList.tsx
@@ -0,0 +1,16 @@
+import React, { useEffect } from 'react';
+
+const VideoList = () => {
+
+    useEffect(() => {
+
+    }, [])
+
+    return (
+        <div>
+            Video List
+        </div>
+    );
+};
+
+export default VideoList;
