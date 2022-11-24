69f422e734cceede9558ffe5fbffa7e2916ec98a
gabrielTettamanti <gabrieltettamanti.dev@gmail.com>
2022-11-24 10:14:32 -0300

    The Rest API is made and running

--- API/src/controllers/videos.controllers.ts
+++ API/src/controllers/videos.controllers.ts
@@ -10,15 +10,26 @@ export const createVideo: RequestHandler = async (req, res) => {
     const savedVideo = await video.save();
     res.json(savedVideo);
 };
-export const getVideos: RequestHandler = (req, res) => {
-    res.json('getting videos')
+export const getVideos: RequestHandler = async (req, res) => {
+    try {
+        const videos = await Video.find()
+        return res.json(videos);
+    } catch (error){
+        res.json(error);
+    };
 };
-export const getVideo: RequestHandler = (req, res) => {
-    res.json('getting a single video')
+export const getVideo: RequestHandler = async (req, res) => {
+    const videoFound = await Video.findById(req.params.id);
+    if(!videoFound) return res.status(204).json();
+    return res.json(videoFound);
 };
-export const deleteVideo: RequestHandler = (req, res) => {
-    res.json('deleting a single video')
+export const deleteVideo: RequestHandler = async (req, res) => {
+    const videoFound = await Video.findByIdAndDelete(req.params.id);
+    if(!videoFound) return res.status(204).json();
+    return res.json(videoFound);
 };
-export const updateVideo: RequestHandler = (req, res) => {
-    res.json('updating a single video')
+export const updateVideo: RequestHandler = async (req, res) => {
+    const videoUpdated = await Video.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if(!videoUpdated) return res.status(204).json();
+    return res.json(videoUpdated);
 };
