2d0d673b3d483d8a1cd1e70fe37f8dc1fb46e3c9
gabrielTettamanti <gabrieltettamanti.dev@gmail.com>
2022-11-30 15:44:14 -0300

    Added Create new video form and made it the service

--- client/src/components/Video/VideoForm.tsx
+++ client/src/components/Video/VideoForm.tsx
@@ -1,11 +1,73 @@
-import React from 'react';
+import React, { FormEvent, ChangeEvent, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import * as VideoServices from "./VideoServices";
+import { Video } from "./Video";
+
+type InputChange = ChangeEvent<HTMLInputElement | HTMLTextAreaElement>;
 
 const VideoForm = () => {
-    return (
-        <div>
-            Video Form
+
+    const history = useNavigate()
+
+    const [video, setVideo] = useState<Video>({title:'', description:'', url: ''});
+
+    const handleInputChange = (e: InputChange) => {
+        setVideo({...video, [e.target.name]: e.target.value})
+    };
+
+    const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const res = await VideoServices.createVideo(video);
+        toast.success('New video added');
+        history('/');
+    };
+
+  return (
+    <div className="row">
+      <div className="col-md-4 offset-md-4">
+        <div className="card">
+          <div className="card-body">
+            <h3>New video</h3>
+
+            <form onSubmit={handleSubmit}>
+              <div className="form-group">
+                <input
+                  type="text"
+                  name="title"
+                  placeholder="Write a title for this video"
+                  className="form-control"
+                  onChange={handleInputChange}
+                  autoFocus
+                />
+              </div>
+              <div className="form-group">
+                <input
+                  type="text"
+                  name="url"
+                  placeholder="https://somesite.com"
+                  className="form-control"
+                  onChange={handleInputChange}
+                />
+              </div>
+              <div className="form-group">
+                <textarea
+                  name="description"
+                  rows={3}
+                  className="form-control"
+                  placeholder="Write a description"
+                  onChange={handleInputChange}
+                ></textarea>
+              </div>
+              <button className="btn btn-primary">
+                Create video
+              </button>
+            </form>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default VideoForm;
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+
 import VideoList from './components/Video/VideosList';
 import VideoForm from './components/Video/VideoForm';
 import Navbar from './components/Navbar/Navbar';
+
+import 'react-toastify/dist/ReactToastify.css';
 import './index.css';
 import 'bootswatch/dist/pulse/bootstrap.min.css';
 
@@ -17,18 +22,16 @@ root.render(
 
       <Navbar />
 
-      <div className="container">
+      <div className="container p-4">
         <Routes>
           <Route path='/' element={<VideoList />} />
           <Route path='/new-video' element={<VideoForm />} />
         </Routes>
+        <ToastContainer />
       </div>
 
     </BrowserRouter>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
